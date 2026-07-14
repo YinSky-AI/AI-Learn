0245e2c75f8d3d008aa602dc33a3c8d75416bba9
yinsky <1966547521@qq.com>
2026-07-14 20:41:59 +0800

    chore: remove unused ruoyi framework, design refs, test artifacts and dev intermediates

--- backend/init-db.sql
+++ backend/init-db.sql
@@ -4,7 +4,3 @@
 -- 启用必要的扩展
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE EXTENSION IF NOT EXISTS "pg_trgm";
-
--- 若依管理后台 schema
-CREATE SCHEMA IF NOT EXISTS ruoyi;
-GRANT ALL ON SCHEMA ruoyi TO postgres;
